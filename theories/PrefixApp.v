647ec99f13d023a27e1f518e9b1a581400aa7d5e
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-01-19 05:19:12 +0000

    Split prefixApp* lemmas into a separate file

---
+++ theories/PrefixApp.v
@@ -0,0 +1,20 @@
+From stdpp Require Import numbers list.
+
+Lemma prefixOfAppSingleton {A : Type} (l1 l2 : list A) (x : A) : l1 `prefix_of` l2 ++ [x] -> l1 `prefix_of` l2 \/ l1 = l2 ++ [x].
+Proof.
+  unfold prefix. intro h.
+  destruct h as [w h].
+  induction w using rev_ind.
+  - right. rewrite app_nil_r in h. easy.
+  - left. exists w. pose proof app_inj_tail l2 (l1 ++ w) x x0 as H.
+    rewrite <- app_assoc in H. tauto.
+Qed.
+
+Lemma prefixAppCases {A : Type} (l x y : list A) (h : l `prefix_of` x ++ y) : l `prefix_of` x \/ exists l', l = x ++ l'.
+Proof.
+  induction y using rev_ind.
+  - left. rewrite app_nil_r in h. tauto.
+  - rewrite app_assoc in h. pose proof (prefixOfAppSingleton _ _ _ h). destruct H.
+    * tauto.
+    * rewrite H. right. exists (y ++ [x0]). rewrite app_assoc. easy.
+Qed.
--- theories/RegularBracketString.v
+++ theories/RegularBracketString.v
@@ -1,6 +1,6 @@
 From stdpp Require Import numbers list.
 Require Import Wellfounded.
-From CoqCP Require Import ExistsInRange.
+From CoqCP Require Import ExistsInRange PrefixApp.
 
 Inductive Bracket :=
 | BracketOpen
@@ -170,25 +170,6 @@ Qed.
 
 Lemma isBalancedBoolIffBalanceFactorBasedDefinition (s : list Bracket) : (isBalancedBool s = true) <-> balanceFactorBasedDefinition s.
 Proof. unfold isBalancedBool. rewrite <- ifSubstituteZero. apply isBalancedBoolAuxIffWithInitialBalanceFactor. Qed.
-
-Lemma prefixOfAppSingleton {A : Type} (l1 l2 : list A) (x : A) : l1 `prefix_of` l2 ++ [x] -> l1 `prefix_of` l2 \/ l1 = l2 ++ [x].
-Proof.
-  unfold prefix. intro h.
-  destruct h as [w h].
-  induction w using rev_ind.
-  - right. rewrite app_nil_r in h. easy.
-  - left. exists w. pose proof app_inj_tail l2 (l1 ++ w) x x0 as H.
-    rewrite <- app_assoc in H. tauto.
-Qed.
-
-Lemma prefixAppCases {A : Type} (l x y : list A) (h : l `prefix_of` x ++ y) : l `prefix_of` x \/ exists l', l = x ++ l'.
-Proof.
-  induction y using rev_ind.
-  - left. rewrite app_nil_r in h. tauto.
-  - rewrite app_assoc in h. pose proof (prefixOfAppSingleton _ _ _ h). destruct H.
-    * tauto.
-    * rewrite H. right. exists (y ++ [x0]). rewrite app_assoc. easy.
-Qed.
 
 Lemma isBalancedImpliesBalanceFactorBasedDefinition (s : list Bracket) : isBalanced s -> balanceFactorBasedDefinition s.
 Proof.
