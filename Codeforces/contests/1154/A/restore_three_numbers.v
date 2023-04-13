9fd1e684b738e7ffb564688537cdade40f39c449
JPhoenix <11325345+JPhoenix@users.noreply.github.com>
2023-04-13 02:07:13 -0600

    improved example proofs

--- Codeforces/contests/1154/A/restore_three_numbers.v
+++ Codeforces/contests/1154/A/restore_three_numbers.v
@@ -24,13 +24,8 @@ Qed.
 
 (* Define the inputs *)
 
-Record input_x : Type := {
-    input_x_value : Z;
-    input_x_constraints : 2 <= input_x_value <= 1000000000
-}.
-
-Definition valid_input_list(values : list Z) : Prop :=
-  forall (fun x => 2 <= x <= 1000000000) values
+Definition valid_input_list(l : list Z) : Prop :=
+  (forall x, In x l -> 2 <= x <= 1000000000)
   /\
   exists a : Z, 
   exists b : Z, 
@@ -38,7 +33,7 @@ Definition valid_input_list(values : list Z) : Prop :=
   (a >= 1)
   /\ (b >= 1)
   /\ (c >= 1)
-  /\ Permutation [a+b; a+c; b+c; a+b+c] values.
+  /\ Permutation [a+b; a+c; b+c; a+b+c] l.
 
 Record input_list : Type := {
     value : list Z;
@@ -46,39 +41,37 @@ Record input_list : Type := {
 }.
 
 (* Define the algorithm *)
-Definition restore_a_b_c_aux(values : list Z) :=
-  let s := merge_sort Z.le values in
+Definition restore_a_b_c_aux(l : list Z) :=
+  let s := merge_sort Z.le l in
   let a := (nth 3 s 0) - (nth 0 s 0) in
   let b := (nth 3 s 0) - (nth 1 s 0) in
   let c := (nth 3 s 0) - (nth 2 s 0) in
   [a; b; c].
 
-Definition raw_list(values : input_list): list Z :=
-  (map input_x_value (input_list_value values)).
-
-Definition restore_a_b_c (values : input_list) : list Z :=
-  restore_a_b_c_aux(raw_list values).
+Definition restore_a_b_c (l : input_list) : list Z :=
+  restore_a_b_c_aux(value l).
 
 (* Test with input examples *)
 
+Ltac valid_input_list_tactic a b c :=
+  unfold valid_input_list;
+  split;
+  [ intros x H; 
+    repeat (destruct H as [H1 | H]; try lia);
+    contradiction
+  | exists a, b, c; 
+    repeat (split; try lia); 
+    apply z_permutation_by_merge_sort;
+    reflexivity
+  ].
+
+
 (* Example 1 *)
-Lemma input_list_example_1_constraints_proof : 
-  valid_input_list [3; 6; 5; 4].
-Proof.
-  exists 2, 1, 3.
-  repeat split; try lia.
-  apply z_permutation_by_merge_sort.
-  reflexivity.
-Qed.
 
 Definition input_list_example_1 : input_list :=
-  {| input_list_value := [
-    {|input_x_value:=3 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=6 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=5 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=4 ; input_x_constraints := ltac:(lia) |}
-  ];
-  input_list_constraints := input_list_example_1_constraints_proof
+  {| 
+    value := [3; 6; 5; 4];
+    constraints := ltac:(valid_input_list_tactic 2 1 3)
   |}.
 
 Example restore_a_b_c_example_1: restore_a_b_c(input_list_example_1) = [3; 2; 1].
@@ -87,23 +80,10 @@ Proof.
 Qed.
 
 (* Example 2 *)
-Lemma input_list_example_2_constraints_proof : 
-  valid_input_list [40; 40; 40; 60].
-Proof.
-  exists 20, 20, 20.
-  repeat split; try lia.
-  apply z_permutation_by_merge_sort.
-  reflexivity.
-Qed.
-
 Definition input_list_example_2 : input_list :=
-  {| input_list_value := [
-    {|input_x_value:=40 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=40 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=40 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=60 ; input_x_constraints := ltac:(lia) |}
-  ];
-  input_list_constraints := input_list_example_2_constraints_proof
+  {|
+    value := [40; 40; 40; 60];
+    constraints := ltac:(valid_input_list_tactic 20 20 20)
   |}.
 
 Example restore_a_b_c_example_2: restore_a_b_c(input_list_example_2) = [20; 20; 20].
@@ -112,23 +92,10 @@ Proof.
 Qed.
 
 (* Example 3 *)
-Lemma input_list_example_3_constraints_proof : 
-  valid_input_list [201; 101; 101; 200].
-Proof.
-  exists 1, 100, 100.
-  repeat split; try lia.
-  apply z_permutation_by_merge_sort.
-  reflexivity.
-Qed.
-
 Definition input_list_example_3 : input_list :=
-  {| input_list_value := [
-    {|input_x_value:=201 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=101 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=101 ; input_x_constraints := ltac:(lia) |};
-    {|input_x_value:=200 ; input_x_constraints := ltac:(lia) |}
-  ];
-  input_list_constraints := input_list_example_3_constraints_proof
+  {|
+    value := [201; 101; 101; 200];
+    constraints := ltac:(valid_input_list_tactic 1 100 100)
   |}.
 
 Example restore_a_b_c_example_3: restore_a_b_c(input_list_example_3) = [100; 100; 1].
@@ -138,19 +105,23 @@ Qed.
 
 (* Prove the algorithm *)
 
-Definition is_answer_valid(values : input_list): Prop :=
-  let answer := restore_a_b_c(values) in
+Definition is_answer_valid(l : input_list): Prop :=
+  let answer := restore_a_b_c(l) in
   let a := (nth 3 answer 0) - (nth 0 answer 0) in
   let b := (nth 3 answer 0) - (nth 1 answer 0) in
   let c := (nth 3 answer 0) - (nth 2 answer 0) in
   let sums := [a+b; a+c; b+c; a+b+c] in
   length answer = Z.to_nat 3 
-  /\ Permutation sums (map input_x_value (input_list_value values)).
+  /\ Permutation sums (value l).
 
 Theorem solution_is_correct: forall input_l : input_list, is_answer_valid(input_l).
 Proof.
-  intro values.
-  assert 
+  intro l'.
+  destruct l' as [l H].
+  unfold is_answer_valid.
+  split.
+  - reflexivity.
+  -  
   
   
 Qed.
