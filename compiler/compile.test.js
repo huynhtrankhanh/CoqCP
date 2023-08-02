207e9dad85e21c4ce23601499f57118a40fcd7df
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2023-08-02 07:42:48 +0000

    Prettified Code!

--- compiler/compile.test.js
+++ compiler/compile.test.js
@@ -135,31 +135,31 @@ describe('CoqCPASTTransformer', () => {
     const program = `
     procedure("exampleProcedure", { a: int32, b: int32 }, () => {      
         sDivide(get("a"), get("b"));
-    });`;
+    });`
 
-    const transformer = new CoqCPASTTransformer(program);
-    const result = transformer.transform();
+    const transformer = new CoqCPASTTransformer(program)
+    const result = transformer.transform()
 
     expect(result.procedures[0].body[0]).toMatchObject({
       type: 'sDivide',
       left: { type: 'get', name: 'a' },
       right: { type: 'get', name: 'b' },
-    });
-  });
+    })
+  })
 
   it('should parse readInt8 instruction correctly', () => {
     const program = `
     procedure("exampleProcedure", { }, () => {      
         readInt8();
-    });`;
+    });`
 
-    const transformer = new CoqCPASTTransformer(program);
-    const result = transformer.transform();
+    const transformer = new CoqCPASTTransformer(program)
+    const result = transformer.transform()
 
     expect(result.procedures[0].body[0]).toMatchObject({
       type: 'readInt8',
-    });
-  });
+    })
+  })
 
   it('should handle simple if condition correctly', () => {
     const program = `
@@ -167,11 +167,11 @@ describe('CoqCPASTTransformer', () => {
       if(get("a") == 10) {
         get("a");
       }
-    });`;
+    });`
 
-    const transformer = new CoqCPASTTransformer(program);
-    const result = transformer.transform();
-    
+    const transformer = new CoqCPASTTransformer(program)
+    const result = transformer.transform()
+
     expect(result.procedures[0].body[0]).toMatchObject({
       type: 'condition',
       condition: {
@@ -182,12 +182,12 @@ describe('CoqCPASTTransformer', () => {
       },
       body: [{ type: 'get', name: 'a' }],
       alternate: [],
-    });
-  });
+    })
+  })
 
   describe('range() instruction', () => {
     it('should create a range instruction with proper attributes', () => {
-        const code = `environment({
+      const code = `environment({
             loopExample: array([int32], 10)
         });
 
@@ -197,43 +197,47 @@ describe('CoqCPASTTransformer', () => {
             })
         });`
 
-        const transformer = new CoqCPASTTransformer(code);
-        const result = transformer.transform();
+      const transformer = new CoqCPASTTransformer(code)
+      const result = transformer.transform()
 
-        const expectedInstruction = {
-            type: 'range',
-            name: 'x',
-            end: { type: 'literal', value: 10, location: expect.any(Object) },
-            loopVariable: 'x',
-            loopBody: [
-                {
-                    type: 'store',
-                    name: 'loopExample',
-                    index: { type: 'local binder', name: 'x', location: expect.any(Object) },
-                    tuples: [
-                        { type: 'local binder', name: 'x', location: expect.any(Object) }
-                    ],
-                    location: expect.any(Object)
-                }
+      const expectedInstruction = {
+        type: 'range',
+        name: 'x',
+        end: { type: 'literal', value: 10, location: expect.any(Object) },
+        loopVariable: 'x',
+        loopBody: [
+          {
+            type: 'store',
+            name: 'loopExample',
+            index: {
+              type: 'local binder',
+              name: 'x',
+              location: expect.any(Object),
+            },
+            tuples: [
+              { type: 'local binder', name: 'x', location: expect.any(Object) },
             ],
-            location: expect.any(Object)
-        };
+            location: expect.any(Object),
+          },
+        ],
+        location: expect.any(Object),
+      }
 
-        const procBody = result.procedures[0].body;
-       
-        expect(procBody[0]).toEqual(expectedInstruction);
-    });
+      const procBody = result.procedures[0].body
+
+      expect(procBody[0]).toEqual(expectedInstruction)
+    })
 
     it('should throw an error if "range" function does not have two arguments', () => {
-        const codeWithFaultyRange = `
+      const codeWithFaultyRange = `
             environment({ myArray: array([int32], 10) });
             procedure("myProcedure", { i: int32 }, () => {
-                range(10);})`;
-        
-        expect(() => {
-            const transformer = new CoqCPASTTransformer(codeWithFaultyRange);
-            transformer.transform();
-        }).toThrow(/^range\(\) function accepts exactly 2 arguments,/);
-    });
-});
+                range(10);})`
+
+      expect(() => {
+        const transformer = new CoqCPASTTransformer(codeWithFaultyRange)
+        transformer.transform()
+      }).toThrow(/^range\(\) function accepts exactly 2 arguments,/)
+    })
+  })
 })
