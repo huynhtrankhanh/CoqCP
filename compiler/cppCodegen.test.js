6d611897293bdea80794aa4392bacf49b6c65587
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2023-08-04 01:50:47 +0000

    Prettified Code!

--- compiler/cppCodegen.test.js
+++ compiler/cppCodegen.test.js
@@ -5,9 +5,9 @@ const code = `...` // Your existing code
 const transformer = new CoqCPASTTransformer(code)
 
 describe('cppCodegen function', () => {
-    it('should produce correct C++ include statement', () => {
-        const ast = transformer.transform();
-        const cppCode = cppCodegen(ast);
-        expect(cppCode).toEqual('#include<bits/stdc++.h>');
-    });
-});
+  it('should produce correct C++ include statement', () => {
+    const ast = transformer.transform()
+    const cppCode = cppCodegen(ast)
+    expect(cppCode).toEqual('#include<bits/stdc++.h>')
+  })
+})
