5bae12e65b0997235bde48d71911c9476f9e5934
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-08-04 01:50:20 +0000

    Add cppCodegen test

---
+++ compiler/cppCodegen.test.js
@@ -0,0 +1,13 @@
+import { CoqCPASTTransformer } from './parse'
+import { cppCodegen } from './cppCodegen' // replace with actual path to module
+
+const code = `...` // Your existing code
+const transformer = new CoqCPASTTransformer(code)
+
+describe('cppCodegen function', () => {
+    it('should produce correct C++ include statement', () => {
+        const ast = transformer.transform();
+        const cppCode = cppCodegen(ast);
+        expect(cppCode).toEqual('#include<bits/stdc++.h>');
+    });
+});
