7eaba00347770c445a908189161277da3fbb1d69
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-09-18 08:53:26 +0700

    Add noErrors and hasValidationErrorsOnly

--- compiler/validateAST.test.js
+++ compiler/validateAST.test.js
@@ -1,11 +1,12 @@
 // @ts-check
 import { CoqCPASTTransformer, ParseError } from './parse'
 import { validateAST } from './validateAST'
+import { consumeNever } from "./consumeNever"
 /**
  * @param {string} code
  * @returns {import("./combinedError").CombinedError}
  */
-const combined = (code) => {
+const getCombinedError = (code) => {
   const parser = new CoqCPASTTransformer(code)
   try {
     var transformed = parser.transform()
@@ -15,4 +16,26 @@ const combined = (code) => {
     return { type: 'parse error', message: error.message }
   }
   return { type: 'validation error', errors: validateAST(transformed) }
+}
+
+/**
+  * @param {string} code
+  * @returns {boolean}
+  */
+const noErrors = (code) => {
+  const error = getCombinedError(code)
+  if (error.type === "parse error") return false
+  if (error.type === "validation error") return error.errors.length === 0
+  return consumeNever(error.type)
+}
+
+/**
+  * @param {string} code
+  * @returns {boolean}
+  */
+const hasValidationErrorsOnly = (code) => {
+  const error = getCombinedError(code)
+  if (error.type === "parse error") return false
+  if (error.type === "validation error") return error.errors.length !== 0
+  return consumeNever(error.type)
 }
