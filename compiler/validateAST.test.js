6e3e853afb3dec34d03b2f3996ae7131baef7a97
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2023-09-18 01:44:29 +0000

    Prettified Code!

--- compiler/validateAST.test.js
+++ compiler/validateAST.test.js
@@ -1,17 +1,18 @@
 // @ts-check
-import { CoqCPASTTransformer , ParseError} from "./parse"
-import { validateAST } from "./validateAST"
+import { CoqCPASTTransformer, ParseError } from './parse'
+import { validateAST } from './validateAST'
 /**
-  * @param {string} code
-  * @returns {import("./combinedError").CombinedError}
-  */
+ * @param {string} code
+ * @returns {import("./combinedError").CombinedError}
+ */
 const combined = (code) => {
   const parser = new CoqCPASTTransformer(code)
   try {
-  var transformed = parser.transform()
+    var transformed = parser.transform()
   } catch (error) {
-    if(!(error instanceof ParseError) && !(error instanceof SyntaxError)) throw error
-    return { type: "parse error",message: error.message}
+    if (!(error instanceof ParseError) && !(error instanceof SyntaxError))
+      throw error
+    return { type: 'parse error', message: error.message }
   }
-  return {type:"validation error",errors:validateAST(transformed)}
+  return { type: 'validation error', errors: validateAST(transformed) }
 }
