d325f79fd575d709421f96af6a9b3b4edeb1f977
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-09-19 13:24:02 +0000

    Add validator tests

--- compiler/validateAST.test.js
+++ compiler/validateAST.test.js
@@ -56,11 +56,29 @@ describe('validateAST', () => {
       `environment({ fib: array([int32], 0)})`,
       `environment({ intricate: array([bool, int64], 1) })`,
       `procedure("empty", {}, () => {})`,
-      `environment({ a: array([int32], 1) })
+      `environment({ a: array([bool], 1) })
 procedure("hello", { a: bool }, () => {
   store("a", 0, [get("a")])
   set("a", retrieve("a", 0)[0])
 })`,
+      `procedure("hello", {}, () => {
+  range(100, x => { "break" })
+})`,
+      `procedure("hello", {}, () => {
+  range(200, x => { "continue" })
+})`,
+      `procedure("hello", {}, () => {
+  range(200, x => {
+    range(300, x => {
+      "break"
+      if (x == 300) {
+        "break"
+      }
+    })
+  })
+})`,
+      `procedure("hello", {}, () => { writeInt8(coerceInt8(100)) })`,
+      `procedure("hello", {}, () => { writeInt8(coerceInt8(100 + 200)) })`,
     ]
     for (const program of programs) {
       if (!noErrors(program)) {
@@ -71,7 +89,22 @@ procedure("hello", { a: bool }, () => {
     }
   })
   it('rejects invalid code (validate error)', () => {
-    const programs = [`environment({ fractionalLength: array([int64], 10.5) })`]
+    const programs = [
+      `environment({ fractionalLength: array([int64], 10.5) })`,
+      `procedure("hello", {}, () => { "break" })`,
+      `procedure("hello", {}, () => { "continue" })`,
+      `procedure("hello", {}, () => {
+  if (true) {"break"}
+})`,
+      `environment({ a: array([int32], 1) })
+procedure("hello", { a: bool }, () => {
+  store("a", 0, [get("a")])
+  set("a", retrieve("a", 0)[0])
+})`,
+      `procedure("hello", {}, () => { writeInt8(100) })`,
+      `procedure("hello", {}, () => { writeInt8(coerceInt8(coerceInt8(10) + 64))})`,
+      `procedure("hello", {}, () => { if (100) {if (200) {if (300){}}} })`,
+    ]
     for (const program of programs) {
       if (!hasValidationErrorsOnly(program)) {
         console.log('failing program:', program)
@@ -81,7 +114,9 @@ procedure("hello", { a: bool }, () => {
     }
   })
   it('rejects invalid code (parse error)', () => {
-    const programs = []
+    const programs = [
+      `procedure("hello", {}, () => { if(true)if(true)if(true);})`,
+    ]
     for (const program of programs) {
       if (!hasParseErrorsOnly(program)) {
         console.log('failing program:', program)
