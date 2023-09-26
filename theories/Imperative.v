dae763cb606ba1b69e44a15f78ecf054bd6166f3
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-09-26 04:44:34 +0000

    Add initial definition of Action monad

--- theories/Imperative.v
+++ theories/Imperative.v
@@ -1,1 +1,21 @@
 From CoqCP Require Import Options.
+From stdpp Require Import strings.
+Require Import ZArith.
+Open Scope Z_scope.
+Definition plus8 (a b : Z) := (a + b) mod (256 : Z).
+Compute (plus8 100 200).
+
+Record Environment := { arrayType: string -> Type; arrays: forall (name : string), list (arrayType name) }.
+
+Record Locals := { numbers: string -> Z; booleans: string -> bool }.
+
+Inductive Action (environment : Environment) (locals : Locals) :=
+| Done
+| Store (arrayName : string) (index : Z) (value : arrayType environment arrayName) : (Action _ _) -> (Action _ _)
+| Retrieve (arrayName : string) (index : Z) : (arrayType environment arrayName -> (Action _ _)) -> (Action _ _)
+| NumberLocalSet (variableName : string) (value : Z) : (Action _ _) ->  (Action _ _)
+| NumberLocalGet (variableName : string) : (Z -> (Action _ _)) -> (Action _ _)
+| BooleanLocalGet (variableName : string) (value : bool) : (Action _ _) -> (Action _ _)
+| BooleanLocalSet (variableName : string) : (bool -> (Action _ _)) -> (Action _ _)
+| WriteInt8 (output : Z) : (Action _ _) -> (Action _ _)
+| ReadInt8 : (Z -> (Action _ _)) -> (Action _ _).
