98411e602c149059f2359baa89c6aac1b6c8f3f3
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-09-29 04:44:22 +0000

    Add monadic bind

--- theories/Imperative.v
+++ theories/Imperative.v
@@ -13,8 +13,22 @@ Inductive Action (arrayType : string -> Type) :=
 | Retrieve (arrayName : string) (index : Z) : (arrayType arrayName -> Action _) -> Action _
 | NumberLocalSet (variableName : string) (value : Z) : Action _ ->  Action _
 | NumberLocalGet (variableName : string) : (Z -> Action _) -> Action _
-| BooleanLocalGet (variableName : string) (value : bool) : Action _ -> Action _
-| BooleanLocalSet (variableName : string) : (bool -> Action _) -> Action _
-| WriteInt8 (output : Z) : Action _ -> Action _
-| ReadInt8 : (Z -> Action _) -> Action _
+| BooleanLocalSet (variableName : string) (value : bool) : Action _ -> Action _
+| BooleanLocalGet (variableName : string) : (bool -> Action _) -> Action _
+| WriteChar (output : Z) : Action _ -> Action _
+| ReadChar : (Z -> Action _) -> Action _
 | Flush : Action _ -> Action _.
+
+Fixpoint join {arrayType} (a z : Action arrayType) :=
+  match a with
+  | Done _ => z
+  | Store _ a b c next => Store _ a b c (join next z)
+  | Retrieve _ a b next => Retrieve _ a b (fun x => join (next x) z)
+  | NumberLocalSet _ a b next => NumberLocalSet _ a b (join next z)
+  | NumberLocalGet _ a next => NumberLocalGet _ a (fun x => join (next x) z)
+  | BooleanLocalSet _ a b next => BooleanLocalSet _ a b (join next z)
+  | BooleanLocalGet _ a next => BooleanLocalGet _ a (fun x => join (next x) z)
+  | WriteChar _ a next => WriteChar _ a (join next z)
+  | ReadChar _ next => ReadChar _ (fun x => join (next x) z)
+  | Flush _ next => Flush _ (join next z)
+  end.
