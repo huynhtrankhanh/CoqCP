c9bb1cf805f673d65698cbe649d2cbce441cb1c6
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-10-01 08:17:15 +0000

    Avoiding funext requires too much brain

--- theories/Imperative.v
+++ theories/Imperative.v
@@ -1,5 +1,6 @@
 From CoqCP Require Import Options.
 From stdpp Require Import strings.
+Require Import Coq.Logic.FunctionalExtensionality.
 Require Import ZArith.
 Open Scope Z_scope.
 
@@ -19,47 +20,6 @@ Inductive Action (arrayType : string -> Type) :=
 | ReadChar : (Z -> Action _) -> Action _
 | Flush : Action _ -> Action _.
 
-Fixpoint sameAction {arrayType} (a z : Action arrayType) :=
-  match a with
-  | Done _ => z = Done arrayType
-  | Store _ a b c act => match z with
-    | Store _ a' b' c' act' => a = a' /\ b = b' /\ c = c' /\ sameAction act act'
-    | _ => False
-    end
-  | Retrieve _ a b f => match z with
-    | Retrieve _ a' b' f' => a = a' /\ b = b' /\ (forall x, sameAction (f x) (f' x))
-    | _ => False
-    end
-  | NumberLocalSet _ variableName value act => match z with
-    | NumberLocalSet _ variableName' value' act' => variableName = variableName' /\ value = value' /\ sameAction act act'
-    | _ => False
-    end
-  | NumberLocalGet _ variableName f => match z with
-    | NumberLocalGet _ variableName' f' => variableName = variableName' /\ (forall x, sameAction (f x) (f' x))
-    | _ => False
-    end
-  | BooleanLocalSet _ variableName value act => match z with
-    | BooleanLocalSet _ variableName' value' act' => variableName = variableName' /\ value = value' /\ sameAction act act'
-    | _ => False
-    end
-  | BooleanLocalGet _ variableName f => match z with
-    | BooleanLocalGet _ variableName' f' => variableName = variableName' /\ (forall x, sameAction (f x) (f' x))
-    | _ => False
-    end
-  | WriteChar _ output act => match z with
-    | WriteChar _ output' act' => output = output' /\ sameAction act act'
-    | _ => False
-    end
-  | ReadChar _ f => match z with
-    | ReadChar _ f' => forall x, sameAction (f x) (f' x)
-    | _ => False
-    end
-  | Flush _ act => match z with
-    | Flush _ act' => sameAction act act'
-    | _ => False
-    end
-  end.
-
 Fixpoint join {arrayType} (a z : Action arrayType) :=
   match a with
   | Done _ => z
@@ -74,10 +34,10 @@ Fixpoint join {arrayType} (a z : Action arrayType) :=
   | Flush _ next => Flush _ (join next z)
   end.
 
-(* Lemma joinAssoc {arrayType} (x y z :  Action arrayType) : join x (join y z) = join (join x y) z.
+Lemma joinAssoc {arrayType} (x y z :  Action arrayType) : join x (join y z) = join (join x y) z.
 Proof.
   induction x as [| a b c next IH | a b next IH | a b next IH | a next IH | a b next IH | a next IH | a next IH | next IH | next IH] in y, z |- *; try easy; simpl; rewrite ?IH; try reflexivity; pose proof (ltac:(intros; apply functional_extensionality; intros; now rewrite IH) : forall b c : Action arrayType, (fun x => join (next x) (join b c)) = (fun x => join (join (next x) b) c)) as ext; now rewrite ext.
-Qed. *)
+Qed.
 
 Fixpoint rangeLoop {arrayType} (n : nat) (f : nat -> Action arrayType) :=
   match n with
