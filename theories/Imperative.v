b45ccc3c2b19c8adc07e2a5a614f71d565937ac9
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-10-05 03:27:20 +0000

    Rewrite monad code to wrap value

--- theories/Imperative.v
+++ theories/Imperative.v
@@ -8,82 +8,43 @@ Record Environment := { arrayType: string -> Type; arrays: forall (name : string
 
 Record Locals := { numbers: string -> Z; booleans: string -> bool }.
 
-Inductive Action (arrayType : string -> Type) :=
-| Done
-| Store (arrayName : string) (index : Z) (value : arrayType arrayName) : Action _ -> Action _
-| Retrieve (arrayName : string) (index : Z) : (arrayType arrayName -> Action _) -> Action _
-| NumberLocalSet (variableName : string) (value : Z) : Action _ ->  Action _
-| NumberLocalGet (variableName : string) : (Z -> Action _) -> Action _
-| BooleanLocalSet (variableName : string) (value : bool) : Action _ -> Action _
-| BooleanLocalGet (variableName : string) : (bool -> Action _) -> Action _
-| WriteChar (output : Z) : Action _ -> Action _
-| ReadChar : (Z -> Action _) -> Action _
-| Flush : Action _ -> Action _.
+Inductive Action (arrayType : string -> Type) (returnType : Type) :=
+| Done (returnValue : returnType)
+| Store (arrayName : string) (index : Z) (value : arrayType arrayName) : Action _ _ -> Action _ _
+| Retrieve (arrayName : string) (index : Z) : (arrayType arrayName -> Action _ _) -> Action _ _
+| NumberLocalSet (variableName : string) (value : Z) : Action _ _ ->  Action _ _
+| NumberLocalGet (variableName : string) : (Z -> Action _ _) -> Action _ _
+| BooleanLocalSet (variableName : string) (value : bool) : Action _ _ -> Action _ _
+| BooleanLocalGet (variableName : string) : (bool -> Action _ _) -> Action _ _
+| WriteChar (output : Z) : Action _ _ -> Action _ _
+| ReadChar : (Z -> Action _ _) -> Action _ _
+| Flush : Action _ _ -> Action _ _.
 
-Fixpoint join {arrayType} (a z : Action arrayType) :=
+Fixpoint bind {arrayType A B} (a : Action arrayType A) (f : A -> Action arrayType B) : Action arrayType B :=
   match a with
-  | Done _ => z
-  | Store _ a b c next => Store _ a b c (join next z)
-  | Retrieve _ a b next => Retrieve _ a b (fun x => join (next x) z)
-  | NumberLocalSet _ a b next => NumberLocalSet _ a b (join next z)
-  | NumberLocalGet _ a next => NumberLocalGet _ a (fun x => join (next x) z)
-  | BooleanLocalSet _ a b next => BooleanLocalSet _ a b (join next z)
-  | BooleanLocalGet _ a next => BooleanLocalGet _ a (fun x => join (next x) z)
-  | WriteChar _ a next => WriteChar _ a (join next z)
-  | ReadChar _ next => ReadChar _ (fun x => join (next x) z)
-  | Flush _ next => Flush _ (join next z)
+  | Done _ _ value => f value
+  | Store _ _ a b c next => Store _ _ a b c (bind next f)
+  | Retrieve _ _ a b next => Retrieve _ _ a b (fun x => bind (next x) f)
+  | NumberLocalSet _ _ a b next => NumberLocalSet _ _ a b (bind next f)
+  | NumberLocalGet _ _ a next => NumberLocalGet _ _ a (fun x => bind (next x) f)
+  | BooleanLocalSet _ _ a b next => BooleanLocalSet _ _ a b (bind next f)
+  | BooleanLocalGet _ _ a next => BooleanLocalGet _ _ a (fun x => bind (next x) f)
+  | WriteChar _ _ a next => WriteChar _ _  a (bind next f)
+  | ReadChar _ _ next => ReadChar _ _ (fun x => bind (next x) f)
+  | Flush _ _ next => Flush _ _ (bind next f)
   end.
 
-Lemma joinAssoc {arrayType} (x y z :  Action arrayType) : join x (join y z) = join (join x y) z.
+Lemma leftIdentity {arrayType A B} (x : A) (f : A -> Action arrayType B) : bind (Done _ _ x) f = f x.
+Proof. easy. Qed.
+
+Lemma rightIdentity {arrayType A} (x : Action arrayType A) : bind x (Done _ _) = x.
 Proof.
-  induction x as [| a b c next IH | a b next IH | a b next IH | a next IH | a b next IH | a next IH | a next IH | next IH | next IH] in y, z |- *; try easy; simpl; rewrite ?IH; try reflexivity; pose proof (ltac:(intros; apply functional_extensionality; intros; now rewrite IH) : forall b c : Action arrayType, (fun x => join (next x) (join b c)) = (fun x => join (join (next x) b) c)) as ext; now rewrite ext.
+  pose proof (ltac:(intros T next h; now apply functional_extensionality): forall T next, (forall x, bind (next x) (Done arrayType A) = next x) -> (fun (x : T) => bind (next x) (Done arrayType A)) = next) as H.
+  induction x as [| a b c next IH | a b next IH | a b next IH | a next IH | a b next IH | a next IH | a next IH | next IH | next IH]; try easy; simpl; now (rewrite (H _ _ IH) || rewrite IH).
 Qed.
 
-Inductive LoopBodyAction (arrayType : string -> Type) :=
-| ExecuteAction (action : Action arrayType) : LoopBodyAction _ -> LoopBodyAction _
-| Continue
-| Break.
-
-Fixpoint joinLoopBody {arrayType} (a z : LoopBodyAction arrayType) :=
-  match a with
-  | ExecuteAction _ action next => ExecuteAction _ action (joinLoopBody next z)
-  | Continue _ => Continue _
-  | Break _ => Break _
-  end.
-
-Lemma joinLoopBodyAssoc {arrayType} (x y z : LoopBodyAction arrayType) : joinLoopBody x (joinLoopBody y z) = joinLoopBody (joinLoopBody x y) z.
+Lemma assoc {arrayType A B C} (x : Action arrayType A) (f : A -> Action arrayType B) (g : B -> Action arrayType C) : bind x (fun x => bind (f x) g) = bind (bind x f) g.
 Proof.
-  induction x as [a n IH | |] in x, y, z |- *; try easy.
-  simpl. now rewrite IH.
+  pose proof (ltac:(intros T next h; now apply functional_extensionality): forall T next, (forall x, bind (next x) (fun x => bind (f x) g) = bind (bind (next x) f) g) -> (fun (x : T) => bind (next x) (fun x => bind (f x) g)) = (fun x => bind (bind (next x) f) g)) as H.
+  induction x as [| a b c next IH | a b next IH | a b next IH | a next IH | a b next IH | a next IH | a next IH | next IH | next IH]; try easy; simpl; now (rewrite IH || rewrite (H _ _ IH)).
 Qed.
-
-Inductive LoopControl :=
-| KeepGoing
-| Stop.
-
-Fixpoint stringTogether {arrayType} (x : LoopBodyAction arrayType) : Action arrayType :=
-  match x with
-  | ExecuteAction _ action next => join action (stringTogether next)
-  | _ => Done arrayType
-  end.
-
-Fixpoint getLoopControl {arrayType} (x : LoopBodyAction arrayType) : LoopControl :=
-  match x with
-  | ExecuteAction _ _ next => getLoopControl next
-  | Continue _ => KeepGoing
-  | Break _ => Stop
-  end.
-
-Fixpoint rangeLoop {arrayType} (n : nat) (f : nat -> LoopBodyAction arrayType) :=
-  match n with
-  | O => Done arrayType
-  | (S n) => match getLoopControl (f n) with
-    | Stop => (stringTogether (f n))
-    | KeepGoing => join (stringTogether (f n)) (rangeLoop n f)
-    end
-  end.
-
-Definition readCharLoopExample : Action (fun _ => unit) :=
-  rangeLoop 5 (fun _ =>
-    ExecuteAction _ (ReadChar (fun charValue =>
-      if Z.eqb charValue 100 then Break else Continue)) Continue).
