871840d013fe09d30cf81713741cb126374413bf
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-10-19 06:46:08 +0000

    Add initial Coq codegen code

---
+++ compiler/coqCodegen.ts
@@ -0,0 +1,22 @@
+import { CoqCPAST, ValueType } from './parse'
+
+const indent = '  '
+
+const sanitizeName = (name: string): string =>
+  [...name].filter((x) => /[0-9a-zA-Z'_]/.test(x)).join('')
+
+export const coqCodegen = ({ environment, procedures }: CoqCPAST): string => {
+  const procedureNameMap = new Map<string, number>()
+  const sanitizedProcedureNameCollisions = new Map<string, number>()
+
+  const preamble = "From CoqCP Require Import Options Imperative.\n"
+
+  const environmentCode = (() => {
+    if (environment === null) {
+      return `Definition environment : Environment := {| arrayType := fun _ => False; arrays := fun _ => [] |}.`
+    }
+    for (const [name, array] of environment.arrays) {
+
+    }
+  })()
+}
