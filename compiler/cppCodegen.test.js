b208a6d7d73f0426459a6d2b1a6bab52b03a0ed3
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-10-30 02:50:17 +0000

    Fix expected code

--- compiler/cppCodegen.test.js
+++ compiler/cppCodegen.test.js
@@ -62,6 +62,36 @@ describe('cppCodegen function', () => {
     console.log(cppCode)
     expect(cppCode).toEqual(`#include <iostream>
 #include <tuple>
+#include <cstdlib>
+using std::get;
+/**
+ * Author: chilli
+ * License: CC0
+ * Source: Own work
+ * Description: Read an integer from stdin. Usage requires your program to pipe in
+ * input from file.
+ * Usage: ./a.out < input.txt
+ * Time: About 5x as fast as cin/scanf.
+ * Status: tested on SPOJ INTEST, unit tested
+ */
+
+inline uint8_t readChar() { // like getchar()
+  static char buf[1 << 16];
+  static size_t bc, be;
+  if (bc >= be) {
+    buf[0] = 0, bc = 0;
+    be = fread(buf, 1, sizeof(buf), stdin);
+  }
+  return buf[bc++]; // returns 0 on EOF
+}
+
+void writeChar(uint8_t x) {
+  std::cout << (char)x;
+}
+
+void flushSTDOUT() {
+  std::cout << std::flush;
+}
 int8_t toSigned(uint8_t x) { return x; }
 int16_t toSigned(uint16_t x) { return x; }
 int32_t toSigned(uint32_t x) { return x; }
@@ -77,10 +107,13 @@ std::tuple<uint8_t, uint64_t> environment_1[3];
 std::tuple<bool> environment_2[0];
 
 int main() {
+  std::cin.tie(0)->sync_with_stdio(0);
   auto procedure_0 = [&](uint32_t local_0) {
     writeChar(uint8_t(local_0));
   };
-  auto procedure_1 = [&](uint32_t local_0, uint32_t local_1, uint32_t local_2, uint32_t local_3) {
+  auto procedure_1 = [&](uint8_t local_0, uint8_t local_1, uint8_t local_2) {
+  };
+  auto procedure_2 = [&](uint32_t local_0, uint32_t local_1, uint32_t local_2, uint32_t local_3) {
     local_0 = readChar();
     local_1 = uint64_t(0);
     local_2 = uint64_t(1);
@@ -99,6 +132,8 @@ int main() {
     binaryOp([&]() { return toSigned(readChar()); }, [&]() { return toSigned(readChar()); }, [&](auto a, auto b) { return a / b; });
     procedure_0(uint64_t(100));
     procedure_0(0);
+    ([&]() {auto workaround_0 = readChar(); auto workaround_1 = 0; auto workaround_2 = readChar(); procedure_1(workaround_0, workaround_1, workaround_2);})();
+    procedure_1(uint8_t(uint64_t(0)), 0, uint8_t(uint64_t(0)));
     if ((local_0 == uint64_t(100))) {
       writeChar(uint64_t(32));
     }
