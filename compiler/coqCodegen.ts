7d1816a4d3bd7d95c0f8dca7291a7fba630c0b1e
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-11-22 07:44:44 +0700

    Add Coq string construction code

--- compiler/coqCodegen.ts
+++ compiler/coqCodegen.ts
@@ -1,5 +1,23 @@
 import { CoqCPAST, ValueType } from './parse'
 
+const getCoqString = (text: string): string => {
+const encoder = new TextEncoder();
+
+function getByteCode(string) {
+  const utf8Bytes = encoder.encode(string);
+  return Array.from(utf8Bytes).map(byte => `"${byte.toString().padStart(3, '0')}"`);
+}
+
+function constructCoqString(byteCode) {
+  return byteCode.reduceRight((acc, code) => `String ${code} (${acc})`, 'EmptyString');
+}
+
+const byteCode = getByteCode(text);
+const coqString = constructCoqString(byteCode);
+
+return(coqString);
+}
+
 const indent = '  '
 
 const sanitizeName = (name: string): string =>
