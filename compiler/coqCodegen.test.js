81ec5fa61a28884ffc9b295748eb760939eeadf8
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-12-05 10:19:18 +0000

    Delete coqCodegen.test.js

--- compiler/coqCodegen.test.js
+++
@@ -1,9 +0,0 @@
-import { transformer } from './exampleCode'
-import { coqCodegen } from './coqCodegen'
-describe('coqCodegen function', () => {
-  it('should produce correct Coq code', () => {
-    const ast = transformer.transform()
-    console.log(coqCodegen(ast))
-    expect(false).toBe(true)
-  })
-})
