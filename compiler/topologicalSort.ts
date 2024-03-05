794838af5362fa6f8f1e7633d92964fbfeac28de
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2024-03-05 09:05:40 +0000

    Prettified Code!

--- compiler/topologicalSort.ts
+++ compiler/topologicalSort.ts
@@ -1,34 +1,34 @@
 export function topologicalSort(graph: Map<number, number[]>) {
-    const visited = new Set<number>();
-    const result: number[] = [];
+  const visited = new Set<number>()
+  const result: number[] = []
 
-    function dfs(node: number) {
-        visited.add(node);
-        const neighbors = graph.get(node) || [];
-        for (const neighbor of neighbors) {
-            if (!visited.has(neighbor)) {
-                dfs(neighbor);
-            }
-        }
-        result.push(node);
+  function dfs(node: number) {
+    visited.add(node)
+    const neighbors = graph.get(node) || []
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        dfs(neighbor)
+      }
     }
+    result.push(node)
+  }
 
-    for (const node of graph.keys()) {
-        if (!visited.has(node)) {
-            dfs(node);
-        }
+  for (const node of graph.keys()) {
+    if (!visited.has(node)) {
+      dfs(node)
     }
+  }
 
-    return result.reverse();
+  return result.reverse()
 }
 
 function edgeListToGraph(edgeList: [number, number][]): Map<number, number[]> {
-    const graph = new Map<number, number[]>();
-    for (const [src, dest] of edgeList) {
-        if (!graph.has(src)) {
-            graph.set(src, []);
-        }
-        graph.get(src)?.push(dest);
+  const graph = new Map<number, number[]>()
+  for (const [src, dest] of edgeList) {
+    if (!graph.has(src)) {
+      graph.set(src, [])
     }
-    return graph;
+    graph.get(src)?.push(dest)
+  }
+  return graph
 }
