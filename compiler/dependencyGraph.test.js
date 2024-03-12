64ea7e905210f43ad21569c947dfa9ec38b34038
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-03-12 10:08:58 +0700

    Test findDependencies

--- compiler/dependencyGraph.test.js
+++ compiler/dependencyGraph.test.js
@@ -1,5 +1,6 @@
 // @ts-check
-import { CoqCPAST, ParseError } from './parse'
+import { CoqCPAST, ParseError, Procedure } from './parse'
+import { findDependencies } from './dependencyGraph';
 
 /**
  * Creates a CoqCPAST object with the given moduleName and dependencies.
@@ -43,3 +44,76 @@ function createEdges(moduleName, dependencies) {
 
   return coqCPAST
 }
+
+/**
+ * Tests for the findDependencies function
+ */
+
+/**
+ * Test case when CoqCPAST has no procedures
+ */
+test('No procedures in CoqCPAST', () => {
+  const x = new CoqCPAST();
+  const dependencies = findDependencies(x);
+  expect(dependencies).toEqual([]);
+});
+
+/**
+ * Test case when CoqCPAST has procedures with no cross module calls
+ */
+test('Procedures with no cross module calls', () => {
+  const procedure1: Procedure = {
+    name: 'procedure1',
+    variables: new Map<string, PrimitiveType>(),
+    body: [{ type: 'set', name: 'x', value: { type: 'literal', valueType: 'number', raw: '42', location: { start: { line: 1, column: 0 }, end: { line: 1, column: 2 } } } }],
+  };
+  const procedure2: Procedure = {
+    name: 'procedure2',
+    variables: new Map<string, PrimitiveType>(),
+    body: [{ type: 'get', name: 'x', location: { start: { line: 1, column: 0 }, end: { line: 1, column: 2 } } }],
+  };
+  const x = new CoqCPAST();
+  x.procedures = [procedure1, procedure2];
+  const dependencies = findDependencies(x);
+  expect(dependencies).toEqual([]);
+});
+
+/**
+ * Test case when CoqCPAST has procedures with cross module calls
+ */
+test('Procedures with cross module calls', () => {
+  const procedure1: Procedure = {
+    name: 'procedure1',
+    variables: new Map<string, PrimitiveType>(),
+    body: [{ type: 'cross module call', procedure: 'proc2', module: 'module2', presetVariables: new Map<string, ValueType>(), arrayMapping: new Map<string, string>(), location: { start: { line: 1, column: 0 }, end: { line: 1, column: 2 } } }],
+  };
+  const procedure2: Procedure = {
+    name: 'procedure2',
+    variables: new Map<string, PrimitiveType>(),
+    body: [{ type: 'get', name: 'x', location: { start: { line: 1, column: 0 }, end: { line: 1, column: 2 } } }],
+  };
+  const x = new CoqCPAST();
+  x.procedures = [procedure1, procedure2];
+  const dependencies = findDependencies(x);
+  expect(dependencies).toEqual(['module2']);
+});
+
+/**
+ * Test case when CoqCPAST has circular dependencies
+ */
+test('Circular dependencies in CoqCPAST', () => {
+  const procedure1: Procedure = {
+    name: 'procedure1',
+    variables: new Map<string, PrimitiveType>(),
+    body: [{ type: 'cross module call', procedure: 'proc2', module: 'module2', presetVariables: new Map<string, ValueType>(), arrayMapping: new Map<string, string>(), location: { start: { line: 1, column: 0 }, end: { line: 1, column: 2 } } }],
+  };
+  const procedure2: Procedure = {
+    name: 'procedure2',
+    variables: new Map<string, PrimitiveType>(),
+    body: [{ type: 'cross module call', procedure: 'proc1', module: 'module1', presetVariables: new Map<string, ValueType>(), arrayMapping: new Map<string, string>(), location: { start: { line: 1, column: 0 }, end: { line: 1, column: 2 } } }],
+  };
+  const x = new CoqCPAST();
+  x.procedures = [procedure1, procedure2];
+  const dependencies = findDependencies(x);
+  expect(dependencies).toEqual(['module1', 'module2']);
+});
