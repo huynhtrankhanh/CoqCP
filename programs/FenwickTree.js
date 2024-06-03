aa910283efcbd7c11e4be6b26ad6ba4bc1343a3b
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-06-03 16:19:53 +0700

    Update FenwickTree to better fit CSES problem statement

--- programs/FenwickTree.js
+++ programs/FenwickTree.js
@@ -1,75 +1,96 @@
 environment({
   fenwick: array([int64], 200001),
   resultArray: array([int64], 1),
-  queryType: array([int64], 1),
-  queryIndex: array([int64], 2),
-  queryValue: array([int64], 1),
+  tempArray: array([int64], 1),
+  idxArray: array([int64], 1),
+  valueArray: array([int64], 1),
+  sumArray: array([int64], 1),
   printBuffer: array([int8], 20),
+  n: int64,
+  q: int64
 })
 
 procedure('increase', { idx: int64, value: int64 }, () => {
-  set('idx', get('idx') + 1) // Convert 0-based index to 1-based index for Fenwick Tree
+  store('idxArray', 0, [get('idx') + 1]) // Convert 0-based index to 1-based index for Fenwick Tree
+  store('valueArray', 0, [get('value')])
 
   range(30, (i) => {
-    if (less(200000, get('idx'))) {
-      ;('break')
+    if (less(get('n'), retrieve('idxArray', 0)[0])) {
+      'break'
     }
-    store('fenwick', get('idx'), [
-      retrieve('fenwick', get('idx'))[0] + get('value'),
+    store('fenwick', retrieve('idxArray', 0)[0], [
+      retrieve('fenwick', retrieve('idxArray', 0)[0])[0] + retrieve('valueArray', 0)[0]
     ])
-    set('idx', get('idx') + (get('idx') & -get('idx')))
+    store('idxArray', 0, [retrieve('idxArray', 0)[0] + (retrieve('idxArray', 0)[0] & -retrieve('idxArray', 0)[0])])
   })
 })
 
-procedure('query', { idx: int64, sum: int64 }, () => {
-  set('sum', 0)
-  set('idx', get('idx') + 1) // Convert 0-based index to 1-based index for Fenwick Tree
+procedure('query', { idx: int64 }, () => {
+  store('sumArray', 0, [0])
+  store('idxArray', 0, [get('idx') + 1]) // Convert 0-based index to 1-based index for Fenwick Tree
 
   range(30, (i) => {
-    if (sLess(get('idx'), 0)) {
-      ;('break')
+    if (less(retrieve('idxArray', 0)[0], 1)) {
+      'break'
     }
-    set('sum', get('sum') + retrieve('fenwick', get('idx'))[0])
-    set('idx', get('idx') - (get('idx') & -get('idx')))
+    store('sumArray', 0, [retrieve('sumArray', 0)[0] + retrieve('fenwick', retrieve('idxArray', 0)[0])[0]])
+    store('idxArray', 0, [retrieve('idxArray', 0)[0] - (retrieve('idxArray', 0)[0] & -retrieve('idxArray', 0)[0])])
   })
 
-  store('resultArray', 0, [get('sum')])
+  store('resultArray', 0, [retrieve('sumArray', 0)[0]])
 })
 
-procedure('rangeQuery', { left: int64, right: int64, result: int64 }, () => {
+procedure('rangeQuery', { left: int64, right: int64 }, () => {
   call('query', { idx: get('right') })
-  set('result', retrieve('resultArray', 0)[0])
+  store('tempArray', 0, [retrieve('resultArray', 0)[0]])
 
   call('query', { idx: get('left') - 1 })
-  set('result', get('result') - retrieve('resultArray', 0)[0])
+  store('tempArray', 0, [retrieve('tempArray', 0)[0] - retrieve('resultArray', 0)[0]])
 
-  store('resultArray', 0, [get('result')])
+  store('resultArray', 0, [retrieve('tempArray', 0)[0]])
 })
 
-procedure(
-  'main',
-  { t: int64, idx: int64, value: int64, left: int64, right: int64 },
-  () => {
-    call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
-    range(retrieve('resultArray', 0)[0], (i) => {
-      call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
-      set('t', retrieve('resultArray', 0)[0])
-      call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
-      set('idx', retrieve('resultArray', 0)[0])
+procedure('update', { idx: int64, value: int64 }, () => {
+  // Get current value at idx
+  call('rangeQuery', { left: get('idx'), right: get('idx') })
+  store('tempArray', 0, [retrieve('resultArray', 0)[0]])
 
-      if (get('t') == 0) {
-        call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
-        set('value', retrieve('resultArray', 0)[0])
-        call('increase', { idx: get('idx'), value: get('value') })
-      } else {
-        call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
-        set('right', retrieve('resultArray', 0)[0])
-        call('rangeQuery', { left: get('idx'), right: get('right') })
-        call(PrintInt64, { buffer: 'printBuffer' }, 'unsigned', {
-          num: retrieve('resultArray', 0)[0],
-        })
-        writeChar(coerceInt8(10)) // Print newline
-      }
-    })
-  }
-)
+  // Calculate the delta
+  store('tempArray', 0, [get('value') - retrieve('tempArray', 0)[0]])
+
+  // Apply the delta
+  call('increase', { idx: get('idx'), value: retrieve('tempArray', 0)[0] })
+})
+
+procedure('main', { }, () => {
+  call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
+  store('n', 0, [retrieve('resultArray', 0)[0]])
+  call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
+  store('q', 0, [retrieve('resultArray', 0)[0]])
+
+  range(retrieve('n', 0)[0], (i) => {
+    call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
+    call('increase', { idx: i, value: retrieve('resultArray', 0)[0] })
+  })
+
+  range(retrieve('q', 0)[0], (i) => {
+    call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
+    store('tempArray', 0, [retrieve('resultArray', 0)[0]])
+    call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
+    store('idxArray', 0, [retrieve('resultArray', 0)[0]])
+
+    if (retrieve('tempArray', 0)[0] == 1) {
+      call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
+      store('valueArray', 0, [retrieve('resultArray', 0)[0]])
+      call('update', { idx: retrieve('idxArray', 0)[0] - 1, value: retrieve('valueArray', 0)[0] })
+    } else {
+      call(ReadUnsignedInt64, { resultArray: 'resultArray' }, '', {})
+      store('tempArray', 0, [retrieve('resultArray', 0)[0]])
+      call('rangeQuery', { left: retrieve('idxArray', 0)[0] - 1, right: retrieve('tempArray', 0)[0] - 1 })
+      call(PrintInt64, { buffer: 'printBuffer' }, 'unsigned', {
+        num: retrieve('resultArray', 0)[0],
+      })
+      writeChar(coerceInt8(10)) // Print newline
+    }
+  })
+})
