5d3ae743562c3244efe02c7778b13002f338fef6
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-06-03 09:29:53 +0700

    Create PrintInt64.js

---
+++ programs/IO/PrintInt64.js
@@ -0,0 +1,39 @@
+module(PrintInt64)
+// Create a buffer to hold characters
+environment({
+  buffer: array([int8], 20)
+})
+
+procedure('unsigned', { num: int64, i: int64, tmpChar: int8 }, () => {
+  // Handle special case for 0
+  if (get('num') == 0) {
+    writeChar(48)
+    ;('return')
+  }
+
+  set('i', 0)
+
+  // Extract digits and store in buffer
+  range(20, (j) => {
+    if (get('num') == 0) {
+      ;('break')
+    }
+    set('tmpChar', coerceInt8(get('num') % 10 + 48))
+    store('buffer', get('i'), [get('tmpChar')])
+    set('num', get('num') / 10)
+    set('i', get('i') + 1)
+  })
+
+  // Print digits in reverse order
+  range(get('i'), (j) => {
+    writeChar(retrieve('buffer', get('i') - j - 1)[0])
+  })
+})
+
+procedure('signed', { num: int64 }, () => {
+  if (sLess(get('num'), 0)) {
+    set('num', -get('num'))
+    writeChar(45) // Print '-'
+  }
+  call('unsigned', { num: get('num') })
+})
