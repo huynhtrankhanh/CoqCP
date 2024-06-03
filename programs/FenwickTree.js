8e1878ce0cec800ebd990033c65c3a714dae13d2
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-06-03 09:40:39 +0700

    Fix while loops

--- programs/FenwickTree.js
+++ programs/FenwickTree.js
@@ -12,24 +12,26 @@ environment({
 procedure('increase', { idx: int64, value: int64 }, () => {
   set('idx', get('idx') + 1) // Convert 0-based index to 1-based index for Fenwick Tree
 
-  while (get('idx') <= 200000) {
+  range(30, i => {
+  if (less(200000, get('idx'))) { 'break' }
     set(
       'fenwick',
       get('idx'),
       retrieve('fenwick', get('idx'))[0] + get('value')
     )
     set('idx', get('idx') + (get('idx') & -get('idx')))
-  }
+  })
 })
 
 procedure('query', { idx: int64, sum: int64 }, () => {
   set('sum', 0)
   set('idx', get('idx') + 1) // Convert 0-based index to 1-based index for Fenwick Tree
 
-  while (get('idx') > 0) {
+  range(30, i => {
+    if (sLess(get('idx', 0))) { "break" }
     set('sum', get('sum') + retrieve('fenwick', get('idx'))[0])
     set('idx', get('idx') - (get('idx') & -get('idx')))
-  }
+  })
 
   store('resultArray', 0, [get('sum')])
 })
@@ -62,9 +64,7 @@ procedure(
         call('ReadSignedInt64', { resultArray: 'resultArray' }, '', {})
         set('right', retrieve('resultArray', 0)[0])
         call('rangeQuery', { left: get('idx'), right: get('right') })
-        call('PrintInt64', { buffer: 'printBuffer' }, 'unsigned', {
-          num: retrieve('resultArray', 0)[0],
-        })
+        call('PrintInt64', { buffer: 'printBuffer' }, 'unsigned', { num: retrieve('resultArray', 0)[0] })
         writeChar(10) // Print newline
       }
     })
