2101407337b452c4145b9585878d636f126cff37
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-06-03 09:31:54 +0700

    Create FenwickTree.js

---
+++ programs/FenwickTree.js
@@ -0,0 +1,62 @@
+module(FenwickTree)
+
+environment({
+  fenwick: array([int64], 200001),
+  resultArray: array([int64], 1),
+  queryType: array([int64], 1),
+  queryIndex: array([int64], 2),
+  queryValue: array([int64], 1)
+})
+
+procedure('increase', { idx: int64, value: int64 }, () => {
+  set('idx', get('idx') + 1) // Convert 0-based index to 1-based index for Fenwick Tree
+
+  while (get('idx') <= 200000) {
+    set('fenwick', get('idx'), retrieve('fenwick', get('idx'))[0] + get('value'))
+    set('idx', get('idx') + (get('idx') & -get('idx')))
+  }
+})
+
+procedure('query', { idx: int64, sum: int64 }, () => {
+  set('sum', 0)
+  set('idx', get('idx') + 1) // Convert 0-based index to 1-based index for Fenwick Tree
+
+  while (get('idx') > 0) {
+    set('sum', get('sum') + retrieve('fenwick', get('idx'))[0])
+    set('idx', get('idx') - (get('idx') & -get('idx')))
+  }
+
+  store('resultArray', 0, [get('sum')])
+})
+
+procedure('rangeQuery', { left: int64, right: int64, result: int64 }, () => {
+  call('query', { idx: get('right') })
+  set('result', get('resultArray')[0])
+
+  call('query', { idx: get('left') - 1 })
+  set('result', get('result') - get('resultArray')[0])
+
+  store('resultArray', 0, [get('result')])
+})
+
+procedure('processQueries', { q: int64, t: int64, idx: int64, value: int64, left: int64, right: int64 }, () => {
+  range(get('q'), (i) => {
+    call('readSigned64BitInteger', {})
+    set('t', retrieve('resultArray', 0)[0])
+    call('readSigned64BitInteger', {})
+    set('idx', retrieve('resultArray', 0)[0])
+    
+    if (get('t') == 0) {
+      call('readSigned64BitInteger', {})
+      set('value', retrieve('resultArray', 0)[0])
+      call('increase', { idx: get('idx'), value: get('value') })
+    } else {
+      call('readSigned64BitInteger', {})
+      set('right', retrieve('resultArray', 0)[0])
+      call('rangeQuery', { left: get('idx'), right: get('right') })
+      call('PrintInt64', {})
+      call('printUnsignedInt64', { num: retrieve('resultArray', 0)[0] })
+      writeChar(10) // Print newline
+    }
+  })
+})
