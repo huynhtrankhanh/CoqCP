ed3e1e7d41ac5e848d14f64033d438925ddc609c
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2024-08-17 07:06:51 +0000

    Prettified Code!

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -1,6 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-};
+  solidity: '0.8.24',
+}
