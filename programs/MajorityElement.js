7220e92ef749397504eaf38a97ab505dfb051a68
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2024-08-19 10:50:14 +0000

    Prettified Code!

--- programs/MajorityElement.js
+++ programs/MajorityElement.js
@@ -2,12 +2,12 @@ environment({
   current: array([int32], 1),
   count: array([int32], 1),
   scratchpad: array([int8], 1024),
-  member: array([int256], 200)
+  member: array([int256], 200),
 })
 
 procedure('main', { what: int32, absolute: address }, () => {
   // extract number from calldata
-  store("member", 0, [2])
+  store('member', 0, [2])
   set(
     'what',
     (coerceInt32(retrieve(0)) << coerceInt32(24)) +
@@ -16,44 +16,49 @@ procedure('main', { what: int32, absolute: address }, () => {
       coerceInt32(retrieve(3))
   )
 
-  if (retrieve("count", 0)[0] == coerceInt32(0)) {
-    store("current", 0, [get("what")])
+  if (retrieve('count', 0)[0] == coerceInt32(0)) {
+    store('current', 0, [get('what')])
   }
 
-  if (get("what") == retrieve("current", 0)[0]) {
-    store("count", 0, [retrieve("count", 0)[0] + coerceInt32(1)])
+  if (get('what') == retrieve('current', 0)[0]) {
+    store('count', 0, [retrieve('count', 0)[0] + coerceInt32(1)])
   } else {
-    store("count", 0, [retrieve("count", 0)[0] - coerceInt32(1)])
+    store('count', 0, [retrieve('count', 0)[0] - coerceInt32(1)])
   }
 
-  store(0, retrieve("count", 0)[0] >> coerceInt32(24))
-  store(1, retrieve("count", 0)[0] >> coerceInt32(16) & coerceInt32(255))
-  store(2, retrieve("count", 0)[0] >> coerceInt32(8) & coerceInt32(255))
-  store(3, retrieve("count", 0)[0] & coerceInt32(255))
+  store(0, retrieve('count', 0)[0] >> coerceInt32(24))
+  store(1, (retrieve('count', 0)[0] >> coerceInt32(16)) & coerceInt32(255))
+  store(2, (retrieve('count', 0)[0] >> coerceInt32(8)) & coerceInt32(255))
+  store(3, retrieve('count', 0)[0] & coerceInt32(255))
 
-  donate(get("absolute"), coerceInt256(2000))
-  invoke(get("absolute"), coerceInt256(2000), "scratchpad", coerceInt64(1024))
-  invoke(getSender(), getMoney(), "scratchpad", coerceInt64(1024))
-  invoke(address(
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0),
-    coerceInt8(0)
-  ), getMoney(), "scratchpad", coerceInt64(1024))
+  donate(get('absolute'), coerceInt256(2000))
+  invoke(get('absolute'), coerceInt256(2000), 'scratchpad', coerceInt64(1024))
+  invoke(getSender(), getMoney(), 'scratchpad', coerceInt64(1024))
+  invoke(
+    address(
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0),
+      coerceInt8(0)
+    ),
+    getMoney(),
+    'scratchpad',
+    coerceInt64(1024)
+  )
 })
