2252713f23555dd703296cfb21e464040622abf9
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-08-21 22:35:37 +0700

    Update solidityCodegen.ts

--- compiler/solidityCodegen.ts
+++ compiler/solidityCodegen.ts
@@ -1,6 +1,9 @@
 import { CoqCPAST } from './parse'
 
 export const solidityCodegen = (sortedModules: CoqCPAST[]): string => {
+  const crossModuleProcedureMap = new PairMap<string, string, Procedure>()
+  const procedureNameMap = new PairMap<string, string, number>()
+  const seenModules = new Map<string, CoqCPAST>()
   const mainModule = sortedModules.find((x) => x.moduleName === '')
 
   let joined =
