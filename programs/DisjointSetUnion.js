6205b836f427e7b9a4828b824059dd146e422b0f
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2024-09-07 08:27:27 +0000

    Prettified Code!

--- programs/DisjointSetUnion.js
+++ programs/DisjointSetUnion.js
@@ -1,12 +1,14 @@
 environment({
   dsu: array([int8], 100),
   hasBeenInitialized: array([int8], 1),
-  result: array([int8], 1)
+  result: array([int8], 1),
 })
 
 procedure('ancestor', { vertex: int8 }, () => {
-  range(100, _ => {
-    if (sLess(retrieve('dsu', vertex)[0], coerceInt8(0))) { "break" }
+  range(100, (_) => {
+    if (sLess(retrieve('dsu', vertex)[0], coerceInt8(0))) {
+      ;('break')
+    }
     set('vertex', retrieve('dsu', vertex)[0])
   })
   store('result', [get('vertex')])
