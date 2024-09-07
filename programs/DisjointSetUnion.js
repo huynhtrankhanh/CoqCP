5ac0a541a9dc69f1133224957fe1b036f54838d6
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-09-07 15:09:31 +0700

    Update DisjointSetUnion.js

--- programs/DisjointSetUnion.js
+++ programs/DisjointSetUnion.js
@@ -3,4 +3,11 @@ environment({
   hasBeenInitialized: array([int8], 0),
 })
 
-procedure('main', {}, () => {})
+procedure('main', {}, () => {
+  if (retrieve('hasBeenInitialized', 0)[0] == coerceInt8(0)) {
+    store('hasBeenInitialized', 0, coerceInt8(1))
+    range(100, i => {
+      store('dsu', i, [coerceInt8(-1)])
+    })
+  }
+})
