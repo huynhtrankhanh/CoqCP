0b4642244d69b00b3299810f3e7e2c2659295631
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-09-13 05:21:43 +0000

    Work on list of trees

--- theories/DisjointSetUnion.v
+++ theories/DisjointSetUnion.v
@@ -90,3 +90,80 @@ Proof.
       lia. }
     pose proof h (leafCount a) (leafCount b) ltac:(assumption) ltac:(assumption). lia.
 Qed.
+
+Fixpoint constructTree (n : nat) : Tree :=
+  match n with
+  | O => Unit
+  | S n => Unite (constructTree n) Unit
+  end.
+
+Lemma constructTreeSize (n : nat) : leafCount (constructTree n) = S n.
+Proof.
+  induction n as [| n IH].
+  - easy.
+  - simpl. lia.
+Qed.
+
+Fixpoint generateProduct {A B : Type} (a : list A) (b : list B) : list (A * B) :=
+  match a with
+  | [] => []
+  | head :: tail => map (fun x => (head, x)) b ++ generateProduct tail b
+  end.
+
+Lemma inProductList {A B : Type} (a : list A) (b : list B) (elementA : A) (hA : In elementA a) (elementB : B) (hB : In elementB b) : In (elementA, elementB) (generateProduct a b).
+Proof.
+  induction a as [| head tail IH].
+  - simpl in hA. exfalso. exact hA.
+  - destruct hA as [hL | hR].
+    + subst head. simpl. apply in_or_app. left. clear IH.
+      induction b as [| head1 tail1 IH1]; [(simpl in hB); exfalso; exact hB |]. simpl. destruct hB as [h1 | h2]. { left. rewrite h1. trivial. } right. exact (IH1 h2).
+    + simpl. apply in_or_app. right. exact (IH hR).
+Qed.
+
+Lemma inProductList2 {A B : Type} (a : list A) (b : list B) (elementA : A) (elementB : B) (h : In (elementA, elementB) (generateProduct a b)) : In elementA a /\ In elementB b.
+Proof.
+  induction a as [| head tail IH].
+  - simpl in h. exfalso. exact h.
+  - simpl in h. rewrite in_app_iff in h. destruct h as [h | h].
+    + clear IH. induction b as [| head1 tail1 IH1]. { simpl in h. exfalso. exact h. } simpl in h. destruct h as [h | h]. { rewrite pair_eq in h. destruct h as [hA hB]. subst head. subst head1. split; simpl; left; trivial. } pose proof IH1 h as [hA hB]. split. { assumption. } simpl. right. assumption.
+    + pose proof IH h as [hA hB]. split. { simpl. right. assumption. } assumption.
+Qed.
+
+Lemma enumerateTrees (n : nat) : { x : list Tree | forall tree : Tree, leafCount tree = n <-> In tree x }.
+Proof.
+  induction (lt_wf n) as [n _ IH].
+  - destruct (decide (n = 0)) as [h | h].
+    + apply (exist _ []). subst n. intros tree. pose proof oneLeqLeafCount tree. simpl. split; lia.
+    + destruct (decide (n = 1)) as [h1 | h1].
+      * subst n. apply (exist _ [Unit]). intros tree.
+        destruct tree as [| tree1 tree2]. { simpl. intuition tauto. }
+        simpl. pose proof oneLeqLeafCount tree1. pose proof oneLeqLeafCount tree2. split. { lia. } intro h1. destruct h1 as [h1 | h1]. { pose proof (ltac:(easy) : Unit <> Unite tree1 tree2) h1. exfalso. assumption. } exfalso. assumption.
+      * assert (h2 : forall (leftSize : nat) (hLeft : leftSize < n) (hLeft2 : 0 < leftSize), {x : list Tree | forall tree : Tree, (match tree with | Unit => False | Unite a b => leafCount a <= leftSize /\ leafCount b = n - leafCount a end) <-> In tree x}).
+        { intros left hLeft hLeft2.
+          induction left as [| left IH2]; [lia |].
+          pose proof IH (S left) ltac:(lia) as [a ha].
+          pose proof IH (n - S left) ltac:(lia) as [b hb].
+          destruct (decide (left = 0)) as [h2 | h2].
+          - apply (exist _ (map (fun (x : Tree * Tree) => let (a, b) := x in Unite a b) (generateProduct a b))).
+            intros [| a1 b1]. { split; intro hTree. { exfalso. exact hTree. } remember (generateProduct a b) as l eqn:hl. induction l as [| head tail IH3] in hTree |-. { simpl in hTree. exact hTree. } destruct hTree as [hTree | hTree]. { destruct head as [aa bb]. exact ((ltac:(easy) : Unite aa bb <> Unit) hTree). } tauto. }
+            pose proof oneLeqLeafCount a1. rewrite (ltac:(lia) : leafCount a1 <= S left <-> leafCount a1 = S left).
+            split.
+            { intros [hTree1 hTree2].
+              pose proof in_map (fun (x : Tree * Tree) => let (a, b) := x in Unite a b) (generateProduct a b) (a1, b1) as step. simpl in step. apply step. apply inProductList. { rewrite <- ha. assumption. } rewrite <- hb. lia. }
+            { rewrite in_map_iff. intros [[aa bb] [h4 h3]].
+              pose proof inProductList2 a b aa bb h3 as [hIn1 hIn2]. rewrite <- ha in hIn1. injection h4. intros hbb haa. subst aa bb. split. { exact hIn1. } pose proof hb b1 as hb1. rewrite hIn1, hb1. assumption. }
+          - pose proof IH2 ltac:(lia) ltac:(lia) as [previous hPrevious]. apply (exist _ (map (fun (x : Tree * Tree) => let (a, b) := x in Unite a b) (generateProduct a b) ++ previous)). intros [| aa bb].
+            + split. { tauto. } intro h3. pose proof in_app_or _ _ _ h3 as [H | H].
+              * remember (generateProduct a b) as l eqn:hl. induction l as [| head tail IH3] in H |-. { simpl in H. exact H. }
+                destruct H as [H | H]. { destruct head as [aa bb]. exact ((ltac:(easy) : Unite aa bb <> Unit) H). } exact (IH3 H).
+              * pose proof hPrevious Unit as [_ step]. exact (step H).
+            + split.
+              * intros [haa hbb]. apply in_or_app.
+                pose proof (ltac:(lia) : leafCount aa <= left \/ leafCount aa = S left) as [hIf | hIf].
+                { right. rewrite <- (hPrevious (Unite aa bb)). simpl. lia. }
+                left. rewrite in_map_iff. exists (aa, bb). split. { trivial. } apply inProductList. { rewrite <- ha. lia. } { rewrite <- hb. lia. }
+              * intro hmap. pose proof in_app_or _ _ _ hmap as [H | H].
+                { rewrite in_map_iff in H. destruct H as [[a1 b1] [hinj hin]]. injection hinj. intros hbb haa. subst bb aa. pose proof inProductList2 _ _ _ _ hin as [hina hinb]. rewrite <- ha in hina. split. { lia. } rewrite hina. rewrite hb. exact hinb. }
+                pose proof hPrevious (Unite aa bb) as step. simpl in step. rewrite <- step in H. lia. }
+          pose proof h2 (n - 1) ltac:(lia) ltac:(lia) as [l hl].
+          apply (exist _ l).
