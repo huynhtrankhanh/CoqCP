2cfe42b81aa6da839f8f271a0218853e88118d01
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-09-17 16:38:43 +0700

    Fix syntax

--- compiler/cli.ts
+++ compiler/cli.ts
@@ -159,7 +159,7 @@ function main() {
         process.exit(1)
         return
       }
-      compile(newInputs.map(x => path.join(folderPath,x),path.join(folderPath,coqOutput),path.join(folderPath,solidityOutput),true)
+      compile(newInputs.map(x => path.join(folderPath,x)),path.join(folderPath,coqOutput),path.join(folderPath,solidityOutput),true)
     } else if (type === 'competitive') {
       const { inputs, cppOutput, coqOutput } = content
       if (!Array.isArray(inputs)) {
@@ -186,7 +186,7 @@ function main() {
         process.exit(1)
         return
       }
-        compile(newInputs.map(x => path.join(folderPath,x),path.join(folderPath,coqOutput),path.join(folderPath,cppOutput),true)
+        compile(newInputs.map(x => path.join(folderPath,x)),path.join(folderPath,coqOutput),path.join(folderPath,cppOutput),true)
     
     } else {
       console.log('Unrecognized type')
