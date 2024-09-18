737e305cb40d0962811017cf1d8e244a2722cbae
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-09-18 04:44:00 +0000

    Define interaction function

--- theories/DisjointSetUnionCode.v
+++ theories/DisjointSetUnionCode.v
@@ -6,3 +6,15 @@ Definition state : BlockchainState := fun address =>
   if decide (address = repeat (0%Z) 20) then
     BlockchainContract arrayIndex0 _ (arrayType _ environment0) (arrays _ environment0) 100000%Z (funcdef_0__main (fun x => false) (fun x => 0%Z) (fun x => repeat 0%Z 20))
   else ExternallyOwned 0%Z.
+
+Fixpoint interact (state : BlockchainState) (interaction : list (Z * Z)) :=
+  match interaction with
+  | [] => getBalance (state (repeat 1%Z 20))
+  | (a, b) :: tail =>
+    match invokeContractAux (repeat 1%Z 20) (repeat 0%Z 20) 0%Z state state [a; b] 1 with
+    | Some (_, changedState) => interact changedState tail
+    | None => 0%Z
+    end
+  end.
+
+Definition optimalInteraction (x : list (Z * Z)) := forall x', Z.le (interact state x') (interact state x).
