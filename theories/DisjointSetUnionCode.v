037f0ffb2a11abe4bb66795f4f4eb4df8ebac39e
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-10-24 12:04:54 +0000

    Tackle ancestor function

--- theories/DisjointSetUnionCode.v
+++ theories/DisjointSetUnionCode.v
@@ -711,7 +711,375 @@ end
 end)). { apply functional_extensionality_dep. intro x. destruct x; simpl; easy. } rewrite <- hh. clear hh. rewrite !(ltac:(cbv; reflexivity) : (coerceInt (coerceInt (Z.opp 1) 64) 8) = 255%Z) in previous. rewrite previous. rewrite insert_take_drop; [| lia]. rewrite (ltac:(lia) : Z.to_nat (100 - Z.of_nat n - 1) = 100 - S n). rewrite (ltac:(intros; listsEqual) : forall a b c, a ++ b :: c = (a ++ [b]) ++ c). pose proof take_app_length (take (100 - S n) l ++ [255%Z]) (drop (S (100 - S n)) l) as step. rewrite app_length in step. rewrite (ltac:(easy) : length [255%Z] = 1) in step. rewrite take_length in step. rewrite (ltac:(lia) : (100 - S n) `min` length l = 100 - S n) in step. rewrite (ltac:(lia) : 100 - S n + 1 = 100 - n) in step. rewrite step. clear step. rewrite (ltac:(intros; listsEqual) : forall a b c, (a ++ [b]) ++ c = a ++ (b :: c)). rewrite (ltac:(easy) : _ :: repeat _ _ = repeat 255%Z (S n)). case_decide as hIf; [reflexivity |]. pose proof (ltac:(lia) : @length (arrayType arrayIndex0 environment0 arraydef_0__dsu) l <= 100 - S n) as step. simpl in step. rewrite hL in step. lia.
 Qed.
 
-Lemma firstInteraction (a b : Z) : invokeContract (repeat 1%Z 20) (repeat 0%Z 20) 0%Z state state [a; b] 1 = Some ([a; b], stateAfterInteractions (fun x => match x with | arraydef_0__result => [0%Z] | arraydef_0__hasBeenInitialized => [1%Z] | arraydef_0__dsu => convertToArray (unite (repeat (Ancestor Unit) 100) (Z.to_nat a) (Z.to_nat b)) end) (dsuScore (unite (repeat (Ancestor Unit) 100) (Z.to_nat a) (Z.to_nat b)))).
+Lemma runAncestor1 (dsu : list Slot) (hL : length dsu = 100) (h1 : noIllegalIndices dsu) (h2 : withoutCyclesN dsu (length dsu)) (whatever2 a : Z) (hLe1 : Z.le 0 a) (hLt1 : Z.lt a 100) (communication : list Z) continuation whatever n (hN : n < 100) : invokeContractAux (repeat 1%Z 20) (repeat 0%Z 20) 0 state state
+  communication 1 arrayIndex0 arrayIndexEqualityDecidable0
+  (arrayType arrayIndex0 environment0) (λ _0 : arrayIndex0,
+  match
+  _0 as _1
+return
+  (list
+  (arrayType
+  arrayIndex0
+  environment0
+  _1))
+with
+| arraydef_0__dsu =>
+    convertToArray dsu
+| arraydef_0__hasBeenInitialized =>
+    [1%Z]
+| arraydef_0__result =>
+    [whatever]
+end)
+  (funcdef_0__main (λ _ : varsfuncdef_0__main, false)
+  (λ _ : varsfuncdef_0__main, 0%Z)
+  (λ _ : varsfuncdef_0__main, repeat 0%Z 20))
+  (eliminateLocalVariables
+  (λ _ : varsfuncdef_0__ancestor, false) (λ _0 : varsfuncdef_0__ancestor,
+  match _0
+with
+| vardef_0__ancestor_vertex =>
+    whatever2
+| vardef_0__ancestor_work =>
+    a
+end)
+  (λ _ : varsfuncdef_0__ancestor, repeat 0%Z 20)
+  (loop n
+  (λ _ : nat,
+  dropWithinLoop
+  (liftToWithinLoop
+  (numberLocalGet arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor
+  vardef_0__ancestor_work >>=
+λ _0 : withLocalVariablesReturnValue
+  (NumberLocalGet arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor
+  vardef_0__ancestor_work),
+  Done
+  (WithLocalVariables arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor)
+  withLocalVariablesReturnValue Z
+  (coerceInt _0 64) >>=
+λ _1 : Z,
+  retrieve arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor arraydef_0__dsu
+  _1 >>=
+λ _2 : arrayType arrayIndex0 environment0
+  arraydef_0__dsu,
+  Done
+  (WithLocalVariables arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor)
+  withLocalVariablesReturnValue Z
+  (coerceInt 0 8) >>=
+λ _3 : Z,
+  Done
+  (WithLocalVariables arrayIndex0
+  (arrayType arrayIndex0
+  environment0)
+  varsfuncdef_0__ancestor)
+  withLocalVariablesReturnValue bool
+  (bool_decide
+  (toSigned _2 8 < toSigned _3 8)%Z)) >>=
+λ _0 : bool,
+  (if _0
+then
+ break arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor >>=
+λ _ : (),
+  Done
+  (WithinLoop arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor)
+  withinLoopReturnValue ()
+  ()
+else
+ Done
+  (WithinLoop arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor)
+  withinLoopReturnValue ()
+  ()) >>=
+λ _ : (),
+  liftToWithinLoop
+  (numberLocalGet arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor
+  vardef_0__ancestor_work >>=
+λ _1 : withLocalVariablesReturnValue
+  (NumberLocalGet arrayIndex0
+  (arrayType arrayIndex0
+  environment0)
+  varsfuncdef_0__ancestor
+  vardef_0__ancestor_work),
+  Done
+  (WithLocalVariables arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor)
+  withLocalVariablesReturnValue Z
+  (coerceInt _1 64) >>=
+λ _2 : Z,
+  retrieve arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor
+  arraydef_0__dsu _2 >>=
+λ _3 : arrayType arrayIndex0
+  environment0 arraydef_0__dsu,
+  numberLocalSet arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor
+  vardef_0__ancestor_work
+  _3) >>=
+λ _ : (),
+  Done
+  (WithinLoop arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__ancestor)
+  withinLoopReturnValue ()
+  ())) >>= continuation)) = invokeContractAux (repeat 1%Z 20) (repeat 0%Z 20) 0 state state
+  communication 1 arrayIndex0 arrayIndexEqualityDecidable0
+  (arrayType arrayIndex0 environment0) (λ _0 : arrayIndex0,
+  match
+  _0 as _1
+return
+  (list
+  (arrayType
+  arrayIndex0
+  environment0
+  _1))
+with
+| arraydef_0__dsu =>
+    convertToArray dsu
+| arraydef_0__hasBeenInitialized =>
+    [1%Z]
+| arraydef_0__result =>
+    [whatever]
+end)
+  (funcdef_0__main (λ _ : varsfuncdef_0__main, false)
+  (λ _ : varsfuncdef_0__main, 0%Z)
+  (λ _ : varsfuncdef_0__main, repeat 0%Z 20))
+  (eliminateLocalVariables
+  (λ _ : varsfuncdef_0__ancestor, false) (λ _0 : varsfuncdef_0__ancestor,
+  match _0
+with
+| vardef_0__ancestor_vertex =>
+    whatever2
+| vardef_0__ancestor_work =>
+    Z.of_nat (ancestor dsu n (Z.to_nat a))
+end)
+  (λ _ : varsfuncdef_0__ancestor, repeat 0%Z 20) (continuation tt)).
+Proof.
+  revert a hLt1 hLe1. induction n as [| n IH]; intros a hLt1 hLe1.
+  - rewrite (ltac:(simpl; reflexivity) : loop 0 _ = _), (ltac:(simpl; reflexivity) : ancestor dsu 0 _ = _), Z2Nat.id, leftIdentity; [reflexivity | lia].
+  - rewrite (ltac:(simpl; reflexivity) : loop (S _) _ = _). unfold numberLocalGet at 1. rewrite <- !bindAssoc, liftToWithinLoopBind, <- !bindAssoc, dropWithinLoopLiftToWithinLoop, <- !bindAssoc. pose proof @pushNumberGet2 arrayIndex0 (arrayType arrayIndex0 environment0) varsfuncdef_0__ancestor _ (λ _ : varsfuncdef_0__ancestor, false) (λ _1 : varsfuncdef_0__ancestor,
+  match
+  _1
+with
+| vardef_0__ancestor_vertex =>
+    whatever2
+| vardef_0__ancestor_work =>
+    a
+end) (λ _ : varsfuncdef_0__ancestor,
+  repeat
+  0%Z 20) as step. rewrite step. clear step.
+  assert (step : coerceInt a 64 = a).
+  { revert hLe1 hLt1. clear. intros h1 h2. unfold coerceInt. rewrite Z.mod_small. { reflexivity. } lia. }
+  rewrite step, !leftIdentity, liftToWithinLoopBind, <- !bindAssoc, dropWithinLoopLiftToWithinLoop. unfold retrieve at 1. rewrite pushDispatch3.
+Admitted.
+
+Lemma runAncestor (dsu : list Slot) (hL : length dsu = 100) (h1 : noIllegalIndices dsu) (h2 : withoutCyclesN dsu (length dsu)) (a b : Z) (hLe1 : Z.le 0 a) (hLt1 : Z.lt a 100) (hLe2 : Z.le 0 b) (hLt2 : Z.lt b 100) continuation whatever : invokeContractAux (repeat 1%Z 20) (repeat 0%Z 20) 0 state state
+  [a; b] 1 arrayIndex0 arrayIndexEqualityDecidable0
+  (arrayType arrayIndex0 environment0) (λ _0 : arrayIndex0,
+  match
+  _0 as _1
+return
+  (list
+  (arrayType
+  arrayIndex0
+  environment0
+  _1))
+with
+| arraydef_0__dsu =>
+    convertToArray dsu
+| arraydef_0__hasBeenInitialized =>
+    [1%Z]
+| arraydef_0__result =>
+    [whatever]
+end)
+  (funcdef_0__main (λ _ : varsfuncdef_0__main, false)
+  (λ _ : varsfuncdef_0__main, 0%Z)
+  (λ _ : varsfuncdef_0__main, repeat 0%Z 20))
+  (funcdef_0__ancestor (λ _ : varsfuncdef_0__ancestor, false)
+  (update (λ _ : varsfuncdef_0__ancestor, 0%Z)
+  vardef_0__ancestor_vertex a)
+  (λ _ : varsfuncdef_0__ancestor, repeat 0%Z 20) >>= continuation) = invokeContractAux (repeat 1%Z 20) (repeat 0%Z 20) 0 state state
+  [a; b] 1 arrayIndex0 arrayIndexEqualityDecidable0
+  (arrayType arrayIndex0 environment0) (λ _0 : arrayIndex0,
+  match
+  _0 as _1
+return
+  (list
+  (arrayType
+  arrayIndex0
+  environment0
+  _1))
+with
+| arraydef_0__dsu =>
+    convertToArray (pathCompress dsu (length dsu) (Z.to_nat a) (ancestor dsu (length dsu) (Z.to_nat a)))
+| arraydef_0__hasBeenInitialized =>
+    [1%Z]
+| arraydef_0__result =>
+    [Z.of_nat (ancestor dsu (length dsu) (Z.to_nat a))]
+end)
+  (funcdef_0__main (λ _ : varsfuncdef_0__main, false)
+  (λ _ : varsfuncdef_0__main, 0%Z)
+  (λ _ : varsfuncdef_0__main, repeat 0%Z 20))
+  (continuation tt).
+Proof.
+  unfold funcdef_0__ancestor. rewrite !leftIdentity, <- !bindAssoc.
+  assert (hs : update (λ _ : varsfuncdef_0__ancestor, 0%Z) vardef_0__ancestor_vertex a = fun x => match x with | vardef_0__ancestor_vertex => a | _ => 0%Z end).
+  { unfold update. apply functional_extensionality_dep. intro x. destruct x; easy. }
+  rewrite hs. unfold numberLocalGet at 1. autorewrite with advance_program.
+  unfold numberLocalSet at 1. pose proof @pushNumberSet2 arrayIndex0 (arrayType arrayIndex0 environment0) varsfuncdef_0__ancestor _ (λ _ : varsfuncdef_0__ancestor, false) (λ _1 : varsfuncdef_0__ancestor,
+  match _1 with
+| vardef_0__ancestor_vertex =>
+    a
+| vardef_0__ancestor_work =>
+    0%Z
+end) (λ _ : varsfuncdef_0__ancestor,
+  @repeat Z
+  0%Z 20) vardef_0__ancestor_work a as step. rewrite step. clear step hs.
+  assert (hs : (update (λ _0 : varsfuncdef_0__ancestor,
+  match _0
+with
+| vardef_0__ancestor_vertex =>
+    a
+| vardef_0__ancestor_work =>
+    0%Z
+end) vardef_0__ancestor_work
+  a) = fun x => match x with | vardef_0__ancestor_vertex => a | vardef_0__ancestor_work => a end). { apply functional_extensionality_dep. intro x. destruct x; easy. }
+  rewrite hs. clear hs.
+Admitted.
+
+Lemma runUnite (dsu : list Slot) (hL : length dsu = 100) (h1 : noIllegalIndices dsu) (h2 : withoutCyclesN dsu (length dsu)) (a b : Z) (hLt1 : Z.lt a 100) (hLt2 : Z.lt b 100) : invokeContractAux (repeat 1%Z 20) (repeat 0%Z 20) 0 state
+  state [a; b] 1 arrayIndex0 arrayIndexEqualityDecidable0
+  (arrayType arrayIndex0 environment0) (λ _0 : arrayIndex0,
+  match
+  _0 as _1
+return
+  (list
+  (arrayType
+  arrayIndex0
+  environment0
+  _1))
+with
+| arraydef_0__dsu =>
+    convertToArray dsu
+| arraydef_0__hasBeenInitialized =>
+    [1%Z]
+| arraydef_0__result =>
+    [0%Z]
+end)
+  (funcdef_0__main (λ _ : varsfuncdef_0__main, false)
+  (λ _ : varsfuncdef_0__main, 0%Z)
+  (λ _ : varsfuncdef_0__main, repeat 0%Z 20))
+  (eliminateLocalVariables
+  (λ _ : varsfuncdef_0__main, false)
+  (λ _ : varsfuncdef_0__main, 0%Z)
+  (λ _ : varsfuncdef_0__main, repeat 0%Z 20)
+  (liftToWithLocalVariables
+  (funcdef_0__unite
+  (λ _ : varsfuncdef_0__unite, false) (λ _0 : varsfuncdef_0__unite,
+  match _0
+with
+| vardef_0__unite_u =>
+    a
+| vardef_0__unite_v =>
+    b
+| vardef_0__unite_z =>
+    0%Z
+end)
+  (λ _ : varsfuncdef_0__unite, repeat 0%Z 20)) >>=
+λ _ : (),
+  Dispatch
+  (WithLocalVariables arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__main)
+  withLocalVariablesReturnValue
+  (withLocalVariablesReturnValue
+  (DoWithArrays arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__main
+  (Store arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  arraydef_0__result 0 (coerceInt 0 8))))
+  (DoWithArrays arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__main
+  (Store arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  arraydef_0__result 0 (coerceInt 0 8)))
+  (λ _0 : withLocalVariablesReturnValue
+  (DoWithArrays arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__main
+  (Store arrayIndex0
+  (arrayType arrayIndex0
+  environment0)
+  arraydef_0__result 0
+  (coerceInt 0 8))),
+  Done
+  (WithLocalVariables arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__main)
+  withLocalVariablesReturnValue
+  (withLocalVariablesReturnValue
+  (DoWithArrays arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__main
+  (Store arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  arraydef_0__result 0 (coerceInt 0 8))))
+  _0) >>=
+λ _ : (),
+  Done
+  (WithLocalVariables arrayIndex0
+  (arrayType arrayIndex0 environment0)
+  varsfuncdef_0__main)
+  withLocalVariablesReturnValue ()
+  ())) =
+Some
+  ([a; b],
+stateAfterInteractions (λ _0 : arrayIndex0,
+  match
+  _0 as _1
+return
+  (list
+  (arrayType arrayIndex0
+  environment0 _1))
+with
+| arraydef_0__dsu =>
+    convertToArray
+  (unite
+  dsu
+  (Z.to_nat a)
+  (Z.to_nat b))
+| arraydef_0__hasBeenInitialized =>
+    [1%Z]
+| arraydef_0__result => [0%Z]
+end)
+  (dsuScore
+  (unite dsu (Z.to_nat a)
+  (Z.to_nat b)))).
+Proof.
+  unfold funcdef_0__unite. unfold numberLocalGet at 1. rewrite <- !bindAssoc, pushNumberGet2, !leftIdentity, eliminateLift, eliminateLift.
+Admitted.
+
+Lemma firstInteraction (a b : Z) (hLt1 : Z.lt a 100) (hLt2 : Z.lt b 100) : invokeContract (repeat 1%Z 20) (repeat 0%Z 20) 0%Z state state [a; b] 1 = Some ([a; b], stateAfterInteractions (fun x => match x with | arraydef_0__result => [0%Z] | arraydef_0__hasBeenInitialized => [1%Z] | arraydef_0__dsu => convertToArray (unite (repeat (Ancestor Unit) 100) (Z.to_nat a) (Z.to_nat b)) end) (dsuScore (unite (repeat (Ancestor Unit) 100) (Z.to_nat a) (Z.to_nat b)))).
 Proof.
   unfold invokeContract. rewrite (ltac:(easy) : state (repeat 0%Z 20) = BlockchainContract _ _ _ _ _ _). unfold funcdef_0__main at 2. rewrite !leftIdentity. unfold retrieve at 1. rewrite <- !bindAssoc. pose proof pushDispatch2 (λ _ : varsfuncdef_0__main, false) (λ _ : varsfuncdef_0__main, 0%Z) (λ _ : varsfuncdef_0__main, repeat 0%Z 20) (Retrieve arrayIndex0 (arrayType arrayIndex0 environment0) arraydef_0__hasBeenInitialized 0) as step. autorewrite with combined_unfold in step. rewrite step. clear step. autorewrite with advance_program. case_decide as h; simpl in h; [| lia]. rewrite !leftIdentity. case_bool_decide as h1; cbv in h1; [| lia]. unfold store. pose proof pushDispatch2 (λ _ : varsfuncdef_0__main, false) (λ _ : varsfuncdef_0__main, 0%Z) (λ _ : varsfuncdef_0__main, repeat 0%Z 20) (Store arrayIndex0 (arrayType arrayIndex0 environment0) arraydef_0__hasBeenInitialized 0 (coerceInt 1 8)) as step. autorewrite with combined_unfold in *. rewrite <- !bindAssoc, step. clear step. autorewrite with advance_program. assert (hsimp : (λ _0 : arrayIndex0,
   match
@@ -762,6 +1130,7 @@ with
 end) = fun (x : arrayIndex0) => match x with | arraydef_0__hasBeenInitialized => [1%Z] | arraydef_0__dsu => repeat 0%Z 100 | arraydef_0__result => [0%Z] end). { apply functional_extensionality_dep. intro x. destruct x; cbv; reflexivity. }
   case_decide as hw; simpl in hw; [| lia]. rewrite hsimp. clear hsimp. pose proof (fun x => initializeArray a b 100 x ltac:(lia) (repeat 0%Z 100) ltac:(easy)) as step. rewrite step. clear step. rewrite (ltac:(lia) : 100 - 100 = 0). rewrite (ltac:(easy) : take 0 _ = []). rewrite app_nil_l. rewrite !leftIdentity. unfold readByte. pose proof pushDispatch2 (λ _ : varsfuncdef_0__main, false) (λ _ : varsfuncdef_0__main, 0%Z) (λ _ : varsfuncdef_0__main, repeat 0%Z 20) (DoBasicEffect arrayIndex0 (arrayType arrayIndex0 environment0)
   (ReadByte 0)) as step. autorewrite with combined_unfold in step. rewrite step. clear step. autorewrite with advance_program.  case_decide as h2; [| simpl in h2; lia]. pose proof pushDispatch2 (λ _ : varsfuncdef_0__main, false) (λ _ : varsfuncdef_0__main, 0%Z) (λ _ : varsfuncdef_0__main, repeat 0%Z 20) (DoBasicEffect arrayIndex0 (arrayType arrayIndex0 environment0) (ReadByte 1)) as step. autorewrite with combined_unfold in step. rewrite <- !bindAssoc, step. clear step. autorewrite with advance_program. rewrite !leftIdentity. rewrite (ltac:(easy) : (nth (Z.to_nat 0) [a; b] 0%Z) = a), (ltac:(easy) : (nth (Z.to_nat 1) [a; b] 0%Z) = b). rewrite (ltac:(apply functional_extensionality_dep; intro x; destruct x; easy) : (update (update (λ _ : varsfuncdef_0__unite, 0%Z) vardef_0__unite_u a) vardef_0__unite_v b) = fun x => match x with | vardef_0__unite_u => a | vardef_0__unite_v => b | _ => 0%Z end).
+  case_decide as h3; [| simpl in h3; lia].
 Admitted.
 
 Lemma interactEqualsModelScore (x : list (Z * Z)) : interact state x = modelScore x.
