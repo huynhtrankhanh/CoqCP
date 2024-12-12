efe2f4a23928c13ffe197641e9c16e5b1e48d265
huynhtrankhanh <huynhtrankhanh@users.noreply.github.com>
2024-12-12 04:35:25 +0000

    Prettified Code!

--- programs/SimpleProgram.js
+++ programs/SimpleProgram.js
@@ -1,5 +1,7 @@
 environment({ buffer: array([int8], 20) })
 procedure('main', {}, () => {
-  range('The number is ', x => { writeChar(x) });
+  range('The number is ', (x) => {
+    writeChar(x)
+  })
   call(PrintInt64, { buffer: 'buffer' }, 'signed', { num: 1 + 1 })
 })
