137a1d8800abce45e61aba2f016e60f1b2633cbc
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2025-03-17 01:56:55 +0700

    Create BuyLowSellHigh.v

---
+++ theories/BuyLowSellHigh.v
@@ -0,0 +1,9 @@
+From CoqCP Require Import Options Imperative ListsEqual ExistsInRange SwapUpdate.
+From Generated Require Import BuyLowSellHigh.
+From stdpp Require Import numbers list.
+Require Import Coq.Logic.FunctionalExtensionality.
+Require Import Wellfounded.
+From Coq Require Import ssreflect ssrfun ssrbool.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
